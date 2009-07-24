237ef58558e1d49c9a1ad0b2b12cf6a76129f178
Beat Bolli <bb+git@drbeat.li>
2009-07-24 13:59:53 +0200

    Simplify attribute handling

--- xmlbuilder.py
+++ xmlbuilder.py
@@ -33,40 +33,31 @@ class element:
   def __init__(self, name, builder):
     self.name = name
     self.builder = builder
+    self.serialized_attrs = ''
   def __enter__(self):
     self.builder.indentation += 2
-    if hasattr(self, 'attributes'):
-      self.builder.write('<%s %s>\n' % (self.name, self.serialized_attrs))
-    else:
-      self.builder.write('<%s>\n' % self.name)
+    self.builder.write('<%s%s>\n' % (self.name, self.serialized_attrs))
   def __exit__(self, type, value, tb):
     self.builder.write('</%s>\n' % self.name)
     self.builder.indentation -= 2
   def __call__(self, value=_dummy, **kargs):
-    if len(kargs.keys()) > 0:
-      self.attributes = kargs
+    if kargs:
       self.serialized_attrs = self.serialize_attrs(kargs)
     if value == None:
       self.builder.indentation += 2
-      if hasattr(self, 'attributes'):
-        self.builder.write('<%s %s />\n' % (self.name, self.serialized_attrs))
-      else:
-        self.builder.write('<%s />\n' % self.name)
+      self.builder.write('<%s%s />\n' % (self.name, self.serialized_attrs))
       self.builder.indentation -= 2
     elif value != _dummy:
       self.builder.indentation += 2
-      if hasattr(self, 'attributes'):
-        self.builder.write('<%s %s>%s</%s>\n' % (self.name, self.serialized_attrs, value, self.name))
-      else:
-        self.builder.write('<%s>%s</%s>\n' % (self.name, value, self.name))
+      self.builder.write('<%s%s>%s</%s>\n' % (self.name, self.serialized_attrs, value, self.name))
       self.builder.indentation -= 2
       return
     return self
   def serialize_attrs(self, attrs):
     serialized = []
     for attr, value in attrs.items():
-      serialized.append('%s="%s"' % (attr, value))
-    return ' '.join(serialized)
+      serialized.append(' %s="%s"' % (attr, value))
+    return ''.join(serialized)
 
 if __name__ == "__main__":
   xml = builder(version="1.0", encoding="utf-8")
