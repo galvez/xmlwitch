4a4e19b8f87094c0b816a4a31900e32ad0e699a7
galvez <jonas@celine.(none)>
2009-08-02 20:03:17 -0300

    Updated setup.py, renamed xmlbuilder.* to xmlwitch.*

--- setup.py
+++ setup.py
@@ -2,21 +2,24 @@
 
 from distutils.core import setup
 
-DESCRIPTION="A Python implementation of Ruby's XML Builder using the with statement for simplicity"
+DESCRIPTION = "A Python implementation of Ruby's XML Builder using the with statement for simplicity"
 
-setup(name="XMLWitch",
+setup(name="xmlwitch",
       version="0.1.0",
       description=DESCRIPTION,
-      license="GPL",
+      license="BSD",
       author="Jonas Galvez",
       author_email="jonas@codeazur.com.br",
-      url="http://github.com/galvez/xmlwitch/",
-      py_modules = ['xmlbuilder'],
+      url="http://github.com/galvez/xmlwitch/tree/master",
+      py_modules = ['xmlwitch'],
       platforms="Python 2.5 and later",
       classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
         'Topic :: Text Processing :: Markup :: HTML',
-        'Topic :: Text Processing :: Markup :: XML',
+        'Topic :: Text Processing :: Markup :: XML'
         ],)
